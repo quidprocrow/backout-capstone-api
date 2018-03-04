ca7fa1990ff1b179ad3e185ea5f91ef26b9cd540
quidprocrow <windmillwarrior@gmail.com>
2018-03-04 11:50:55 -0500

    Migrate adding step reference to steps
    Check out to experimental branch.
    Migrate adding step reference to sentences.
    Will now tinker with serialization.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304162949) do
+ActiveRecord::Schema.define(version: 20180304164546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,9 @@ ActiveRecord::Schema.define(version: 20180304162949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "game_id"
+    t.bigint "step_id"
     t.index ["game_id"], name: "index_sentences_on_game_id"
+    t.index ["step_id"], name: "index_sentences_on_step_id"
   end
 
   create_table "steps", force: :cascade do |t|
@@ -68,6 +70,7 @@ ActiveRecord::Schema.define(version: 20180304162949) do
 
   add_foreign_key "examples", "users"
   add_foreign_key "sentences", "games"
+  add_foreign_key "sentences", "steps"
   add_foreign_key "steps", "words"
   add_foreign_key "words", "sentences"
 end
