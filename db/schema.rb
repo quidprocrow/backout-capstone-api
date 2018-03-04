7479d98919aae8b1d7a197e6722cc419a646d24c
quidprocrow <windmillwarrior@gmail.com>
2018-03-04 10:21:32 -0500

    Generate words table via the command line
    Generate words table using the scaffold command.
    My plan is to generate the last needed table, steps, then begin connecting
    resources with their intended connections.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304151506) do
+ActiveRecord::Schema.define(version: 20180304152039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 20180304151506) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string "text"
+    t.boolean "clickable"
+    t.boolean "redacted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "examples", "users"
