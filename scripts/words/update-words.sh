d830b276d8f486b2b712e998d8940d7ff85ae865
quidprocrow <windmillwarrior@gmail.com>
2018-03-05 11:33:20 -0500

    Add curl script to update words
    Add curl script to update words.
    Next step is to delete words, move on to steps.

--- scripts/words/update-words.sh
+++ scripts/words/update-words.sh
@@ -2,8 +2,9 @@
 TEXT="test"
 BOOL="false"
 SEN="1"
+ID="2"
 
-curl "http://localhost:4741/words" \
+curl "http://localhost:4741/words/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
